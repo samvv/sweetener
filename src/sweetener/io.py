d4572fb445a502d6d5db4e0d37bce0e8f936f912
Sam Vervaeck <samvv@pm.me>
2023-04-16 16:08:17 +0200

    Add some IO primitives

---
+++ src/sweetener/io.py
@@ -0,0 +1,33 @@
+
+from pathlib import Path
+import sys
+import math
+import shutil
+
+def write_file(filepath: Path, contents: str):
+    with open(filepath, 'w') as file:
+        file.write(contents)
+
+def eprint(message):
+    print(message, file=sys.stderr)
+
+def mkdirp(filepath):
+    Path(filepath).mkdir(parents=True, exist_ok=True)
+
+UNITS = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+
+def humanbytes(bs: int):
+    if bs == 0:
+        return '0B'
+    i = math.floor(math.log(bs, 1024))
+    return f'{(bs / pow(1024, i)):.2f}{UNITS[i]}'
+
+def rimraf(filepath: Path):
+    if filepath == Path.cwd():
+        raise RuntimeError(f'refusing to remove {filepath}: path is the current working directory')
+    if filepath == filepath.root:
+        raise RuntimeError(f'refusing to remove {filepath}: path points to an entire drive')
+    if filepath == Path.home():
+        raise RuntimeError(f'refusing to remove {filepath}: path points to a home directory')
+    shutil.rmtree(filepath, ignore_errors=True)
+
